5f24331845e98565f8ad532307119c387b7f6906
BOZ2323 <lillibaaske@gmx.de>
2016-12-21 14:46:12 +0100

    Added three links to modules in the require part in the beginning of test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var test = require("tape");
-var arrayCompare = require('./index').arrayCompare;
-var tolerantArrayCompare = require('./index').tolerantArrayCompare;
+var arrayCompare = require("number-array-compare");
+var roundNumber = require("round-numbers");
+var tolerantArrayCompare = require("number-array-compare");
 var test = require("tape-catch");
 
 test("fence() should return true. Expected result to be [3,8]", function(t){
@@ -15,7 +16,7 @@ test("fence() should throw an error, if count < 2", function(t){
     t.equal(fence(3,8,1),false);
     t.end();
 });
-test("still run this test inspite of error", function(t){
+test("fence() still runs this test inspite of errortesting the test before", function(t){
     t.equal(1+1,2, "this still ran!");
     t.end();
 });
