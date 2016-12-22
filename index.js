e50e2de0e200b16178353ad6d4438e72787a8c99
nicoHz <nicoHz@posteo.de>
2016-12-22 12:45:29 +0100

    Codereview for test.js and console.log deleting

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function fence(start, end, count) {
         throw new Error("Count must be two or higher");
     }
     var distance = (end-start) / (count - 1);
-    console.log(distance);
 
     var xCoordinates = []; 
     for (var n=0; n<count; n++) {
         var xn = start + n * distance;
         xCoordinates.push(xn);
     }
-    console.log (xCoordinates);
     return xCoordinates;
 }
+
+module.exports = fence;
--- test.js
+++ test.js
@@ -1,34 +1,34 @@
 var test = require('tape');
-var arrayCompare = require('./index').arrayCompare;
+var arrayCompare = require('number-array-compare').arrayCompare;
+var tolerantArrayCompare = require('number-array-compare').tolerantArrayCompare;
+var fence = require('./index');
 
-test("trivialCase: arrayCompare() should return true. Expected result to be [3,8].", function(t) {
-	t.equal(arrayCompare(test_positions, [3,8]), true);
+test("if called with count==2, fence() should return an array containing two values: the start and the end position.", function(t) {
 	var test_positions = fence(3,8,2);
+	t.equal(arrayCompare(test_positions, [3,8]), true);
 	t.end();
 });
 
-test("normalCase: arrayCompae() should return true. Expected fence to return roughly: " + shouldBeResult, function(t) {
-	t.equal(arrayCompare(test_positions, shouldBeResult), true);
-	var test_positions = roundNum(fence(3,8,4));
+
+test("if called with count > 2, fence() should return an array containing the start and the end position and interpolated values.", function(t) {
+	var test_positions = fence(3,8,4);
 	var shouldBeResult = [3,4.67,6.33,8];
+	t.equal(tolerantArrayCompare(test_positions, shouldBeResult, 0.005), true);
 	t.end();
 });
-
-
 
 test("errorCase: Expected thrown error if count < 2", function(t) {
-	t.equal(fence(3,8,1), false);
+	try {
+		fence(3,8,1);
+		t.fail("fence() should have thrown an error but it didn't");
+	} catch(e) {
+		if (e.message.match(/two or higher/)) {
+			t.pass("fence() has thrown an error");
+		} else {
+			t.fail(e.message);
+		}
+	}
 	t.end();
 });
 
 
-/*
-function errorCase() {
-	try { 
-		fence(3,8,1);
-		console.log("Test failed: Expected thrown error if count < 2");
-		// if this line is reached, it means fence did not
-		// throw an error.
-	} catch(err) {}
-}
-*/
